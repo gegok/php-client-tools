d4ef4570a4374c4a62e85d8ef038ba0b37ee83b4
Linar Khayrullin <gegokk@gmail.com>
2014-09-16 11:05:09 +0400

    memcached: use key filter with --list param

--- memcached.php
+++ memcached.php
@@ -30,26 +30,22 @@ $mc->setOption(\Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
 $mc->setOption(\Memcached::OPT_DISTRIBUTION, \Memcached::DISTRIBUTION_CONSISTENT);
 $mc->addServer($host, $port);
 
-if ($listKeys) {
+if (strpos($key, '*') !== false) {
     $allKeys = $mc->getAllKeys();
-    foreach ($allKeys as $key) {
-        echo $key . PHP_EOL;
-    }
-} else {
-    $keys = [];
-    if (strpos($key, '*') !== false) {
-        $allKeys = $mc->getAllKeys();
-        $keyPattern = '/^' . str_replace('*', '.*', $key) . '$/i';
-        foreach ($allKeys as $k) {
-            if (preg_match($keyPattern, $k)) {
-                $keys[] = $k;
-            }
+    $keyPattern = '/^' . str_replace('*', '.*', $key) . '$/i';
+    foreach ($allKeys as $k) {
+        if (preg_match($keyPattern, $k)) {
+            $keys[] = $k;
         }
-    } elseif ($key) {
-        $keys[] = $key;
     }
+} elseif ($key) {
+    $keys[] = $key;
+}
 
-    foreach ($keys as $key) {
+foreach ($keys as $key) {
+    if ($listKeys) {
+        echo $key . PHP_EOL;
+    } else {
         $value = $mc->get($key);
         if (!$value && $mc->getResultCode() == \Memcached::RES_NOTFOUND) {
             echo 'key "' . $key . '" not found' . PHP_EOL;
